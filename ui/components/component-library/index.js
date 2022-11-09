fc1dec4b5fdecbfc8a5f4c30a175f578379f1cef
georgewrmarshall <george.marshall@consensys.net>
2022-11-09 17:23:20 -0600

    Adding global index.js and reordering stylesheets

---
+++ ui/components/component-library/index.js
@@ -0,0 +1,17 @@
+export { AvatarAccount } from './avatar-account';
+export { AvatarFavicon } from './avatar-favicon';
+export { AvatarNetwork } from './avatar-network';
+export { AvatarToken } from './avatar-token';
+export { AvatarWithBadge } from './avatar-with-badge';
+export { BaseAvatar } from './base-avatar';
+export { ButtonBase } from './button-base';
+export { ButtonLink } from './button-link';
+export { ButtonPrimary } from './button-primary';
+export { ButtonSecondary } from './button-secondary';
+export { HelpText } from './help-text';
+export { Icon } from './icon';
+export { Label } from './label';
+export { Tag } from './tag';
+export { Text } from './text';
+export { TextField } from './text-field';
+export { TextFieldBase } from './text-field-base';
